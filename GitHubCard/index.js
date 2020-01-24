b022dd50ad662cb5e4b848b1fbd2014587e9a1a4
Mac <xuromeo@gmail.com>
2020-01-23 19:01:03 -0800

    first try

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/ROMEOXU')
+.then(response => {
+  response.data.forEach(element => {
+    const newcard = createCard(element);
+    allcards.appendChild(newcard);
+  })
+})
+.catch(error=>{
+  console.log('the data was wrong',error)
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -53,3 +62,46 @@ const followersArray = [];
   luishrd
   bigknell
 */
+function createCard(obj){
+  const card = document.createElement('div');
+  const imageofuser = document.createElement('img');
+  const cardinfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  const link = document.createElement('a');
+
+  card.appendChild(imageofuser);
+  card.appendChild(cardinfo);
+  cardinfo.appendChild(name);
+  cardinfo.appendChild(username);
+  cardinfo.appendChild(location);
+  cardinfo.appendChild(profile);
+  cardinfo.appendChild(followers);
+  cardinfo.appendChild(following);
+  cardinfo.appendChild(bio);
+  profile.appendChild(link);
+
+  card.classList.add("card");
+  cardinfo.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  name.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = obj.location;
+  link.href = obj.html_url;
+  followers.textContent = obj.followers;
+  following.textContent = obj.following;
+  bio.textContent = obj.bio;
+
+
+  return card;
+
+
+}
+const allcards = document.querySelector('.cards');
